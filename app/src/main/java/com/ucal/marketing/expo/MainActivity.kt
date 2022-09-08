390b57d33ceb498131bd50c53972d31d5d37670f
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2022-09-08 09:00:18 +0530

    Initial commit Webview implemention

---
+++ app/src/main/java/com/ucal/marketing/expo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ucal.marketing.expo
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainWeb = findViewById<WebView>(R.id.mainWebView)
+
+        mainWeb.webViewClient = WebViewClient()
+
+        mainWeb.apply {
+            loadUrl("file:///android_asset/index.html")
+            settings.javaScriptEnabled = true
+        }
+    }
+}
